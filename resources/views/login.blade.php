d6255c58411ad03eaccc7fb6a5397563bea6ab26
Marco Petersen <m@rcopetersen.com>
2023-12-25 12:43:20 +0100

    Move layout to component

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <a href="{{ route('openid_connect.redirect') }}">Login with Keycloak</a>
-    </body>
-</html>
+<x-layout>
+    <a href="{{ route('openid_connect.redirect') }}">Login with Keycloak</a>
+</x-layout>
