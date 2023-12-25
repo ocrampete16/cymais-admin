e09f58c04e596f714bf4eea0958bb3fe2d612c93
Marco Petersen <m@rcopetersen.com>
2023-12-25 13:06:31 +0100

    Style login form with Bulma

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,15 @@
 <x-layout>
-    <a href="{{ route('openid_connect.redirect') }}">Login with Keycloak</a>
+    <div class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-centered">
+                    <div class="column is-3">
+                        <div class="box">
+                            <a class="button is-fullwidth is-primary" href="{{ route('openid_connect.redirect') }}">Login with Keycloak</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
